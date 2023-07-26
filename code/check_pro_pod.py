709cfa7cd5c0a93d8ba4e4e0a7ab7a459582dd55
surayos sricharoen <surayos@mfec.co.th>
2023-07-26 16:16:28 +0700

    fix

--- code/check_pro_pod.py
+++ code/check_pro_pod.py
@@ -54,10 +54,13 @@ if __name__ == "__main__":
         for col1, col2, col3, col4, col5 in result[0:1]:
                 message_summary += f"<tr><td> {col1} <td>{col2} </td><td>{col3}</td><td>{col4}</td><td>{col5}</td></tr>"
         for col1, col2, col3, col4, col5 in result[1:]:
-                message_summary += f"<tr><td> {col1} <td>{col2} </td><td>{col3}</td><td>{col4}</td><td>{col5}</td></tr>"
+                if int(col4) < 3:
+                   message_summary += f"<tr><td> {col1} <td>{col2} </td><td>{col3}</td><td style=\"color:green;\">{col4}</td><td>{col5}</td></tr>"
+                else:
+                   message_summary += f"<tr><td> {col1} <td>{col2} </td><td>{col3}</td><td style=\"color:red;\">{col4}</td><td>{col5}</td></tr>"
         message_summary += "</table>"
-        #send_message_to_teams_webhook(teams_webhook_url, message_summary)
-        print(message_summary)
+        send_message_to_teams_webhook(teams_webhook_url, message_summary)
+        #print(message_summary)
     #if result is not None:
     #    print("ผลลัพธ์:")
     #    for col1, col3 in result:
