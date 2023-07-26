9a7c2cb6e90de4a8d4d1d07a1f1a9a204900b847
tum007jb <surayose007@gmail.com>
2023-07-26 10:16:37 +0700

    Update send_ocadm.py

--- code/send_ocadm.py
+++ code/send_ocadm.py
@@ -36,6 +36,6 @@ def send_message_to_teams_webhook(webhook_url, message):
 
 if __name__ == "__main__":
     # แทนที่ webhook_url ด้วย URL ของ Microsoft Teams webhook ที่คุณได้รับมา
-    teams_webhook_url = "https://mfeconcloud.webhook.office.com/webhookb2/2eaf140e-1c4c-4a55-b630-67337eafd809@74105ed9-72ff-4685-9154-75f7408b6f67/IncomingWebhook/890b4931533941af9d9adb974966662b/76686ac9-1a87-4a33-a0e4-ce4f4f0031da"
+    teams_webhook_url = ""
     message1 = run_oc_adm_top_node_command()
     send_message_to_teams_webhook(teams_webhook_url, message1)
