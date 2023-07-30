59fb16f25bf39086d66f162f806b8e09f1c06685
Surayos Sricharoen <surayos@mfec.co.th>
2023-07-30 11:13:19 +0700

    fix

--- web/utcc_inventory/loginapp/admin.py
+++ web/utcc_inventory/loginapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from loginapp.models import Users
 # Register your models here.
+admin.site.register(Users)
--- web/utcc_inventory/loginapp/models.py
+++ web/utcc_inventory/loginapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Inventory(models.Model):
+    id_invent = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    model = models.CharField(max_length=200)
+    series_number = models.IntegerField
