5cebb58eb74cd7e86a1fcbf472b6a05dacd6af54
Surayos <surayos@mfec.co.th>
2023-08-02 16:11:59 +0700

    update

---
+++ code/PM_OCP_report.py
@@ -0,0 +1,139 @@
+import subprocess
+import requests
+from datetime import datetime
+
+def run_linux_command_ocadm(command):
+    try:
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+        stdout, stderr = process.communicate()
+
+        if process.returncode == 0:
+            lines = stdout.strip().split('\n')
+            result = []
+            for line in lines:
+                columns = line.split()
+                if len(columns) >= 5:
+                    col1 = columns[0]
+                    col2 = columns[1]
+                    col3 = columns[2]
+                    col4 = columns[3]
+                    col5 = columns[4]
+                    result.append((col1, col2, col3, col4, col5 ))
+            return result
+        else:
+            print(f"เกิดข้อผิดพลาดในการรันคำสั่ง: {stderr}")
+            return None
+
+    except Exception as e:
+        print(f"เกิดข้อผิดพลาด: {e}")
+        return None
+
+def reportadm(result):
+    if result is not None:
+        message_summary = '<figure class="text-center"><blockquote class="blockquote"><h3>Summary Performance OCP</h3></blockquote></figure>'
+        message_summary += '<div class="container d-flex justify-content-center align-items-center"><table class="table table-bordered text-center">'
+        for col1, col2, col3, col4, col5 in result:
+                message_summary += f"<tr><td> {col1} <td>{col2} </td><td>{col3}</td><td>{col4}</td><td>{col5}</td></tr>"
+        message_summary += "</table></div>"
+        return message_summary
+
+def run_linux_command_ocgetco(command):
+    try:
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+        stdout, stderr = process.communicate()
+
+        if process.returncode == 0:
+            lines = stdout.strip().split('\n')
+            result = []
+            for line in lines:
+                columns = line.split()
+                if len(columns) >= 3:
+                    col1 = columns[0]
+                    col3 = columns[2]
+                    col4 = columns[3]
+                    col5 = columns[4]
+                    result.append((col1, col3, col4, col5 ))
+            return result
+        else:
+            print(f"เกิดข้อผิดพลาดในการรันคำสั่ง: {stderr}")
+            return None
+
+    except Exception as e:
+        print(f"เกิดข้อผิดพลาด: {e}")
+        return None
+def reportco(result):
+   if result is not None:
+        message_summary = '<figure class="text-center"><blockquote class="blockquote"><h3>Summary Operator</h3></blockquote></figure>'
+        message_summary += '<div class="container d-flex justify-content-center align-items-center"><table class="table table-bordered text-center">'
+        for col1, col3, col4, col5 in result:
+            if col3 == "True":
+                message_summary += f"<tr><td> {col1} </td><td style=\"color:green;\">{col3}</td><td style=\"color:green;\">{col4}</td><td style=\"color:green;\">{col5}</td></tr>"
+            else:
+                message_summary += f"<tr><td> {col1} </td><td style=\"color:red;\">{col3}</td></td><td style=\"color:red;\">{col4}</td></td><td style=\"color:red;\">{col5}</td></tr>"
+        message_summary += "</table></div>"
+        return message_summary
+
+def run_linux_command_ocgetpo(command):
+    try:
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+        stdout, stderr = process.communicate()
+
+        if process.returncode == 0:
+            lines = stdout.strip().split('\n')
+            result = []
+            for line in lines:
+                columns = line.split()
+                if len(columns) >= 5:
+                    col1 = columns[0]
+                    col2 = columns[1]
+                    col3 = columns[2]
+                    col4 = columns[3]
+                    col5 = columns[4]
+                    result.append((col1, col2, col3, col4, col5 ))
+            return result
+        else:
+            print(f"เกิดข้อผิดพลาดในการรันคำสั่ง: {stderr}")
+            return None
+
+    except Exception as e:
+        print(f"เกิดข้อผิดพลาด: {e}")
+        return None
+
+def reportpod(result,subject):
+    if result is not None:
+        message_summary = f'<figure class="text-center"><blockquote class="blockquote"><h3>Summary Pod {subject} </h3></blockquote></figure>'
+        message_summary += '<div class="container d-flex justify-content-center align-items-center"><table class="table table-bordered text-center">'
+        for col1, col2, col3, col4, col5 in result:
+                message_summary += f"<tr><td> {col1} <td>{col2} </td><td>{col3}</td><td>{col4}</td><td>{col5}</td></tr>"
+        message_summary += "</table></div>"
+        return message_summary
+
+
+if __name__ == "__main__":
+    webintro = ""
+    webintro = '<html><head><title>PM Report </title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></head><body><figure class="text-center"><blockquote class="blockquote"><h1>PM Report Openshift</h1></blockquote><figcaption class="blockquote-footer">By MFEC Infosec Team</figcaption></figure>'
+    webfoot = "</body></html>"
+     # Get OC get co
+    linux_command = "oc get co"
+    result = run_linux_command_ocgetco(linux_command)
+    table1 = reportco(result)
+    # get OC adm top node
+    linux_command = "oc adm top node"
+    result = run_linux_command_ocadm(linux_command)
+    table2 = reportadm(result)
+     # get pod dns
+    linux_command = "oc get po -n openshift-dns"
+    result = run_linux_command_ocgetpo(linux_command)
+    table3 = reportpod(result,"OpenShift-DNS")
+    
+    fullweb = f"{webintro}{table1}{table2}{table3}{webfoot}"
+    
+    #Create file html 
+    now = datetime.now()
+    date_string = now.strftime("%Y-%m-%d")
+    file_name = f"result_{date_string}.html"
+
+    file_path = "/var/www/html/" + file_name
+
+    with open(file_path, "w") as file:
+            file.write(fullweb)
--- code/send_ocadm.py
+++ code/send_ocadm.py
@@ -1,21 +1,30 @@
-import requests
 import subprocess
+import requests
 
-def run_oc_adm_top_node_command():
+def run_linux_command_and_get_columns(command):
     try:
-        # รันคำสั่ง "oc get co" บนระบบปฏิบัติการและรับผลลัพธ์
-        process = subprocess.Popen(['oc', 'adm', 'top', 'node'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
         stdout, stderr = process.communicate()
 
         if process.returncode == 0:
-            message1 = stdout.strip()
-            return message1
+            lines = stdout.strip().split('\n')
+            result = []
+            for line in lines:
+                columns = line.split()
+                if len(columns) >= 5:
+                    col1 = columns[0]
+                    col2 = columns[1]
+                    col3 = columns[2]
+                    col4 = columns[3]
+                    col5 = columns[4]
+                    result.append((col1, col2, col3, col4, col5 ))
+            return result
         else:
-            print(f"Have a Error to run command: {stderr}")
+            print(f"เกิดข้อผิดพลาดในการรันคำสั่ง: {stderr}")
             return None
 
     except Exception as e:
-        print(f"Have a Error: {e}")
+        print(f"เกิดข้อผิดพลาด: {e}")
         return None
 
 def send_message_to_teams_webhook(webhook_url, message):
@@ -35,7 +44,19 @@ def send_message_to_teams_webhook(webhook_url, message):
         print(f"Have a Error: {e}")
 
 if __name__ == "__main__":
-    # แทนที่ webhook_url ด้วย URL ของ Microsoft Teams webhook ที่คุณได้รับมา
-    teams_webhook_url = ""
-    message1 = run_oc_adm_top_node_command()
-    send_message_to_teams_webhook(teams_webhook_url, message1)
+    # แทนที่ command ด้วยคำสั่ง Linux ที่คุณต้องการรัน
+    linux_command = "oc adm top node"
+    teams_webhook_url = "https://mfeconcloud.webhook.office.com/webhookb2/2eaf140e-1c4c-4a55-b630-67337eafd809@74105ed9-72ff-4685-9154-75f7408b6f67/IncomingWebhook/890b4931533941af9d9adb974966662b/76686ac9-1a87-4a33-a0e4-ce4f4f0031da"
+    result = run_linux_command_and_get_columns(linux_command)
+    if result is not None:
+        message_summary = "<h1>Summary Performance OCP</h1>"
+        message_summary += "<table border=\"0\">"
+        for col1, col2, col3, col4, col5 in result:
+                message_summary += f"<tr><td> {col1} <td>{col2} </td><td>{col3}</td><td>{col4}</td><td>{col5}</td></tr>"
+        message_summary += "</table>"
+        send_message_to_teams_webhook(teams_webhook_url, message_summary)
+        #print(message_summary)
+    #if result is not None:
+    #    print("ผลลัพธ์:")
+    #    for col1, col3 in result:
+    #        print(f"คอลัมน์ที่ 1: {col1}, คอลัมน์ที่ 3: {col3}")
